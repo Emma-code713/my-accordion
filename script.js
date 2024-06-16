358fb3ad4a3f123195dc5e6dbc6b6f5bfea97964
unknown <oluwafeminue@gmaile.com>
2024-06-15 17:24:50 -0700

    my accordion

---
+++ script.js
@@ -0,0 +1,6 @@
+var accordion = document.getElementsByClassName('contentbx');
+ for (i = 0; i<accordion.length; i++){
+    accordion[i].addEventListener('click',function(){
+        this.classList.toggle('active')
+    })
+ }
